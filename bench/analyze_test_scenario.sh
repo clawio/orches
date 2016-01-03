617051974b96f9f16b3dd11e3cc0d6da6c5be8b9
root <root@ccmv151.citi.local>
2016-01-03 05:57:44 +0100

    Increase concurrency limits

--- bench/analyze_test_scenario.sh
+++ bench/analyze_test_scenario.sh
@@ -6,7 +6,7 @@ USERNAME=demo
 PASSWORD=demo
 INITIAL="$(echo ${USERNAME} | head -c 1)"
 TESTDIR=/local/users/${INITIAL}/${USERNAME}/testing
-NUMREQ=5000
+NUMREQ=10000
 
 
 # Stat with no depth = PROPFIND with Depth = 0
@@ -25,9 +25,12 @@ OUT=${RESULTDIR}/`date +%Y%m%d%H%M`-$1-$NUMREQ-nochildren
         for i in `ls $OUT/p-$p-n-$NUMREQ-*-nochildren*`; do
             cat $i | tail -n +2 >> $OUT/output-p-$p-n-$NUMREQ-nochildren.csv
         done;
-        cat $OUT/output-p-$p-n-$NUMREQ-nochildren.csv | sort -t"," -g -k 2 > $OUT/output-p-$p-n-$NUMREQ-nochildren.csv-sorted
+        cat $OUT/output-p-$p-n-$NUMREQ-nochildren.csv | sort -g -k 2 > $OUT/output-p-$p-n-$NUMREQ-nochildren.csv-sorted
         mv $OUT/output-p-$p-n-$NUMREQ-nochildren.csv-sorted $OUT/output-p-$p-n-$NUMREQ-nochildren.csv
     done;
+    cp generate_plot.gpl $OUT
+    cd $OUT
+    gnuplot -e "title='$1'" generate_plot.gpl
 }
 
 clawiobench login ${USERNAME} ${PASSWORD}
